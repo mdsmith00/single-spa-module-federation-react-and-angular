c618be67c4651ccd985db4cbb7cebfd19f5004b4
mdsmith00 <mdsmith00@gmail.com>
2021-01-15 13:45:24 +1100

    Fix for angular application not unmounting properly

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import {EmptyRouteComponent} from "./empty-route/empty-route-component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, EmptyRouteComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
